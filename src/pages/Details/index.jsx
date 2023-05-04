7518eabcee9c99131aa6dd4f339f85a5d3b8b586
henrico <henrico.an@gmail.com>
2023-05-04 11:13:45 -0300

    adding styles with styled-component

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export default function App(){
+  return(
+    <Container>
+      <h1>Page Details</h1>
+    </Container>
+  );
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#0D0C0F",
+    BACKGROUND_800: "#262529",
+    BACKGROUND_700: "#3E3B47",
+
+    PINK: "#FF859B",
+    WHITE: "#F4EDE8",
+
+    TEXT_WHITE: "#E5E5E5",
+    TEXT_GRAY: "#999591",
+    
+    GRAY_500: "#312E38",
+    GRAY_300: "#948F99",
+    GRAY_100: "#CAC4CF"
+  }
+}
