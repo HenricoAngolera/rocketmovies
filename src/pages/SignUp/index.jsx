f347586e0f3deb4e705c09ace7633bef08b0a84a
henrico <henrico.an@gmail.com>
2023-06-05 17:58:01 -0300

    user registration

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
 import { BsArrowLeftShort } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api"
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -7,6 +11,35 @@ import { ButtonText } from "../../components/ButtonText";
 
 import { Container, Form, SpaceInput, ButtonBox, Background } from "./styles";
 export function SignUp(){
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Fill all fields")
+    }
+
+    api.post("/users", {
+      name, 
+      email, 
+      password
+    })
+      .then(() => {
+        alert('Successfully registered user!')
+        navigate('/')
+      })
+      .catch((error) => {
+        if(error.response){
+          return alert(error.response.data.message)
+        } else {
+          return alert("Unable to register.")
+        }
+      })
+  }
+
   return (
     <Container>
       <Form>
@@ -19,19 +52,22 @@ export function SignUp(){
             placeholder='Nome'
             type='text'
             icon={FiUser}
+            onChange={e => setName(e.target.value)}
           />
           <Input
             placeholder='E-mail'
             type='text'
             icon={FiMail}
+            onChange={e => setEmail(e.target.value)}
           />
           <Input
             placeholder='Senha'
             type='password'
             icon={FiLock}
+            onChange={e => setPassword(e.target.value)}
           />
         </SpaceInput>
-        <Button title="Entrar" />
+        <Button onClick={handleSignUp}  title="Entrar" />
         <ButtonBox>
           <ButtonText to="/" title="Voltar para o login" icon={BsArrowLeftShort}/>
         </ButtonBox>
